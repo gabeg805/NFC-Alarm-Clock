910625b82101a750221b8f661825b4d5aab1ba96
Gabriel Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-02 16:19:19 -0400

    Broken State: Trying to get RecyclerView and CardView to work.
    - Added useful icons.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
