817c3ee192b848cc99b07419496b8eb63d5e9270
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-05 05:20:14 -0400

    Imported ActionBar.

--- app/src/main/java/com/nfcalarmclock/AddAlarmActivity.java
+++ app/src/main/java/com/nfcalarmclock/AddAlarmActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 
 import android.support.v7.app.AppCompatActivity;
 
+import android.support.v7.app.ActionBar;
 
 public class AddAlarmActivity
     extends AppCompatActivity
