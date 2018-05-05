f9522a46ac2459e36ef857fa313e3bd3cf38e723
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-05 05:27:30 -0400

    Testing ActionBar.

--- app/src/main/java/com/nfcalarmclock/AddAlarmActivity.java
+++ app/src/main/java/com/nfcalarmclock/AddAlarmActivity.java
@@ -19,19 +19,20 @@ public class AddAlarmActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
-        ActionBar actionbar = getSupportActionBar();
-        if (actionbar != null)
-        {
-            actionbar.setDisplayHomeAsUpEnabled(true);
-        }
+        // ActionBar actionbar = getSupportActionBar();
+        // if (actionbar != null)
+        // {
+        //     actionbar.setDisplayHomeAsUpEnabled(true);
+        // }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
-    // @Override
-    // public boolean onSupportNavigateUp()
-    // {  
-    //     finish();  
-    //     return true;  
-    // }
+    @Override
+    public boolean onSupportNavigateUp()
+    {  
+        finish();  
+        return true;  
+    }
 
 }
