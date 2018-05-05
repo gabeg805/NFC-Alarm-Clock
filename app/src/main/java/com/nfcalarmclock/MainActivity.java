4e19d1750a1fbe0acefa7dddafb2a522771efad6
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-04 21:58:39 -0400

    Testing out fragment stuff.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -226,6 +226,8 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void onClick(View view)
                 {
+                    // FragmentManager fragment = getSupportFragmentManager();
+                    // fragment.beginTransaction().replace(R.id.flContent, fragment).commit();
                     Snackbar.make(view, "Replace with your own action",
                                   Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
