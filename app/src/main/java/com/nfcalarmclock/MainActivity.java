74a0a1f48f2899f04adf89afea1ee8e2951fd628
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-05 01:22:28 -0400

    Commented out home up button because app wouldn't start.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity
 
         prepareAlarms();
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     /**
