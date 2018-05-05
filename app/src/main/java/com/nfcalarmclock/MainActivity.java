bb362577fb11f4ef8074ad68e50a2ddd1d2d4486
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-05 04:00:21 -0400

    Still showing back button on main activity. Trying to fix this.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -67,16 +67,16 @@ public class MainActivity extends AppCompatActivity
 
         prepareAlarms();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
-    @Override
-    public boolean onSupportNavigateUp()
-    {  
-        finish();  
-        return true;  
-    }
+    // @Override
+    // public boolean onSupportNavigateUp()
+    // {  
+    //     finish();  
+    //     return true;  
+    // }
 
     @Override
     public void onBackPressed()
