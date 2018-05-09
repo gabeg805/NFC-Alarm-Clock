0b1f11f24c93c691de9ceca95b2dc8b3f6358e3f
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-09 17:34:35 -0400

    Toolbar worked, so adding it as the support bar.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = getToolbar();
-        // setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
         if (getSupportActionBar() == null)
         {
