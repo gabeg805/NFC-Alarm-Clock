2ac3b7f71bf14240d45d63dcfafa6a6e853101be
Gabe Gonzalez <6011521+gabeg805@users.noreply.github.com>
2018-05-09 17:01:24 -0400

    Removed some defaults that were in ab_toolbar.

--- app/src/main/java/com/nfcalarmclock/MainActivity.java
+++ app/src/main/java/com/nfcalarmclock/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Toolbar toolbar = getToolbar();
+        Toolbar toolbar = getToolbar();
         // setSupportActionBar(toolbar);
 
         // if (getSupportActionBar() == null)
