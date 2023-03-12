4a51184d9775b24b1a9a6d5e2cbff096ccb69dc7
gabeg805 <6011521+gabeg805@users.noreply.github.com>
2023-03-11 19:56:35 -0500

    Updated to version 10.7.3 (code 319)
    * Added appupdate/NacAppUpdatedBroadcastReceiver, which is a
      BroadcastReceiver that looks for the MY_PACKAGE_REPLACED intent which
      is called when the app is updated.
    
      This was added because alarms are deleted when an app is updated, so
      the intent needs to be captured so that alarms can be restored.

---
+++ app/src/main/java/com/nfcalarmclock/appupdate/NacAppUpdatedBroadcastReceiver.java
@@ -0,0 +1,36 @@
+package com.nfcalarmclock.appupdate;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.nfcalarmclock.scheduler.NacScheduler;
+
+/**
+ * After the app is updated, reapply the alarms.
+ *
+ * When the app is updated, any alarms that were set are lost. This will attempt to restore those
+ * alarms.
+ */
+public class NacAppUpdatedBroadcastReceiver
+        extends BroadcastReceiver
+{
+
+    /**
+     * It is possible for another actor to send a spoofed intent with no
+     * action string or a different action string and cause undesired behavior.
+     * Ensure that the received Intent's action string matches the expected
+     * value before restoring alarms.
+     */
+    @Override
+    public void onReceive(final Context context, Intent intent)
+    {
+        String action = intent.getAction();
+
+        if (action.equals(Intent.ACTION_MY_PACKAGE_REPLACED))
+        {
+            NacScheduler.updateAll(context);
+        }
+    }
+
+}
