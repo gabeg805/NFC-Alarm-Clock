fdcdbe222ede3081c894f4118104abfedb20289d
gabeg805 <6011521+gabeg805@users.noreply.github.com>
2025-11-21 23:27:25 -0500

    Updated to version 12.6.4 (code 572).
    * In layout/frg_statistics.xml:
    
        - Fixed formatting of the margins so that the ScrollView child has
          the padding, this way the scroll bars are shown at the edge of the
          screen, and the the email and reset buttons have their own
          margins.
    
        - Fixed padding and positioning of the "Statistics started..."
          message.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
 		applicationId = "com.nfcalarmclock"
 		minSdk = 24
 		targetSdk = 35
-		versionCode = 571
-		versionName = "12.6.4-beta004"
+		versionCode = 572
+		versionName = "12.6.4"
 
 		// Set output filename
 		setProperty("archivesBaseName", "nfc_alarm_clock_v${versionName}")
