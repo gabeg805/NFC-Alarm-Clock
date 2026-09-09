26e8356c5ee3303ad8971c7f631fa71d5deb127b
gabeg805 <6011521+gabeg805@users.noreply.github.com>
2026-09-09 19:50:32 -0400

    Updated to version 12.7.1 (code 613).
    * Added res/values*/arrays_whats_new.xml:
    
        - For all translations.
    
    * Added fastlane/metadata/android/en-US/changelogs/613.txt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
 		applicationId = "com.nfcalarmclock"
 		minSdk = 24
 		targetSdk = 36
-		versionCode = 612
-		versionName = "12.7.1-beta004"
+		versionCode = 613
+		versionName = "12.7.1"
 	}
 
 	// Configuration for signing the app on release builds. The keystore.properties file must exist
